678da4f5897432c869c6fb34fc1c1dce249458f6
ypw <915505626@qq.com>
2015-05-21 16:03:18 +0800

    智能停车场

---
+++ smartparking/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace smartparking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 24);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 32);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(502, 56);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(208, 90);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(502, 108);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(208, 90);
+            this.trackBar2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1312, 696);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "smartparking";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.killall);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ smartparking/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Sockets;
+
+namespace smartparking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpListener tcplistener8080;
+
+        public void Send(string a, TcpClient tcpclient)
+        {
+            try
+            {
+                byte[] bs = Encoding.UTF8.GetBytes(a);
+                tcpclient.Client.Send(bs, bs.Length, 0);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        TcpClient client;
+        private void spy8080()
+        {
+            try
+            {
+                tcplistener8080 = new TcpListener(IPAddress.Any, 8080);
+                tcplistener8080.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Spy8080 Failed.");
+                return;
+            }
+
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient tcpclient = tcplistener8080.AcceptTcpClient();
+                    //NetworkStream stream = tcpclient.GetStream();
+                    if (tcpclient != null)
+                    {
+                        client = tcpclient;
+                        //client进入
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        Thread tSpy8080;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tSpy8080 = new Thread(new ThreadStart(spy8080));
+            tSpy8080.IsBackground = true;
+            tSpy8080.Start();
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+            string[] ports = SerialPort.GetPortNames();
+            int i;
+            for (i = 0; i < ports.Length; i++) if (ports[i].Length < 6) comboBox1.Items.Add(ports[i]);
+
+            if (ports.Length == 0)
+            {
+                MessageBox.Show("请连接接收器");
+                //Close();
+            }
+            else
+            {
+                try
+                {
+                    comboBox1.SelectedIndex = 0;
+                    button1_Click(null, null);
+                }
+                catch (Exception ex)
+                {
+                }
+
+            }
+            control = trackBar1.BackColor;
+
+        }
+
+        public void Show(String a)
+        {
+            MessageBox.Show(a);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                serialPort1.PortName = comboBox1.Text;
+                try
+                {
+                    serialPort1.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                button1.Text = "关闭";
+                comboBox1.Enabled = false;
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                button1.Text = "打开";
+                comboBox1.Enabled = true;
+            }
+        }
+        Color control;
+
+        double[] juli = { 0, 0 };
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String rec = serialPort1.ReadLine();
+            label1.Text += rec;
+            if (label1.Text.Length > 200) label1.Text = "";
+            Send(rec + "\n", client);
+            Regex regex = new Regex(@"\[(\d*)\](.*)cm");
+            MatchCollection mc = regex.Matches(rec);
+            try
+            {
+                String reg = mc[0].Groups[1].Value;
+                int id = Convert.ToInt32(reg);
+                reg = mc[0].Groups[2].Value;
+                double d = Convert.ToDouble(reg);
+                juli[id - 1] = d;
+            }catch(Exception ex)
+            {
+
+            }
+
+            String x = "";
+            int i; for (i = 0; i < 2; i++)
+            {
+                x += (i + 1) + "号车数据:" + juli[i] + "\n";
+            }
+            label2.Text = x;
+
+            
+            try
+            {
+                trackBar1.Value = (int)juli[0];
+                if (trackBar1.Value < 7) trackBar1.BackColor = Color.Red;
+                else trackBar1.BackColor = control;
+                trackBar2.Value = (int)juli[1];
+                if (trackBar2.Value < 7) trackBar2.BackColor = Color.Red;
+                else trackBar2.BackColor = control;
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        private void killall(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
